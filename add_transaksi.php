c3da9f673e5b01b6c217bd07b55d08d605de2f2e
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2022-06-05 22:53:46 +0700

    revisi

--- add_transaksi.php
+++ add_transaksi.php
@@ -5,9 +5,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     require_once "koneksi.php";
 
     $idUser = $_POST['user_mobile_id'];
-    $namaUser = $_POST['nama_user'];
+    // $namaUser = $_POST['nama_user'];
     $idWisata = $_POST['wisata_id'];
-    $namaWisata = $_POST['nama_wisata'];
+    // $namaWisata = $_POST['nama_wisata'];
     $tglTiket = $_POST['tgl_tiket'];
     $jumlahTiket = $_POST['jumlah_tiket'];
     $totalHarga = $_POST['total_harga'];
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     $query_tambah_transaksi = "INSERT INTO transaksis VALUES
-        ('', '$idUser', '$namaUser', '$idWisata', '$namaWisata', '$tglTiket', '$jumlahTiket', '$totalHarga','','')";
+        ('', '$idUser', '$idWisata', '$tglTiket', '$jumlahTiket', '$totalHarga','','')";
 
     $result = mysqli_query($koneksi, $query_tambah_transaksi);
     $cek = mysqli_affected_rows($koneksi);
